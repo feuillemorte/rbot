d13ebdbb8b57729b101028d1bdc0b3b955a85ee4
Anna <koshlakova_av@mail.ru>
2017-04-01 17:46:40 +0300

    каркас бота

--- functions/greet_user.py
+++ functions/greet_user.py
@@ -1,13 +1,11 @@
 def greet_user(bot, update):
-	print('AAA')
-	print(update)
-	bot.sendMessage(
-		update.message.chat_id,
-		'Доступные команды:\n\n'
-		'/resolved решена текст\n'
-		'/worktime трудозатраты\n'
-		'/who Кто чем занят?\n'
-		'/myworkчто я делал за день?\n'
-		'/прогнозирование задач\n'
-		'/group статистика отдела\n'
-		)
+    bot.sendMessage(
+        update.message.chat_id,
+        'Доступные команды:\n\n'
+        '/resolved решена текст\n'
+        '/worktime трудозатраты\n'
+        '/who Кто чем занят?\n'
+        '/myworkчто я делал за день?\n'
+        '/прогнозирование задач\n'
+        '/group статистика отдела\n'
+    )
