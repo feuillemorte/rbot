840515e5e0d9f54597f1f62260f6ce7093556d43
Anna <koshlakova_av@mail.ru>
2017-04-08 16:44:51 +0300

    task4

---
+++ functions/who.py
@@ -0,0 +1,19 @@
+from configs.config_reader import get_config
+
+from rm import Rm
+
+config = get_config()
+
+
+def tasks_for_user(bot, update):
+    rm = Rm()
+    text = ''
+    for user_id in config['redmine']['user_ids']:
+        user = rm.redmine.user.get(user_id)
+        text += '<b>{} {}</b>\n\n'.format(user.firstname, user.lastname)
+        for task in user.issues:
+            text += '<a href="{}/issues/{}">[{}]</a> - {} ({})\n'.format(
+                config['redmine']['redmine_url'], task.id, task.id, task.subject, task.status.name
+            )
+
+    bot.sendMessage(update.message.chat_id, text, parse_mode='HTML')
