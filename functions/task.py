2dccab37ddcdedd72d3592e09acdc52c27a1d2d0
Anna Koshlakova <a.koshlakova@dwteam.ru>
2017-04-12 16:29:18 +0300

    Вывод времени, сколько задача была в работе

--- functions/task.py
+++ functions/task.py
@@ -1,6 +1,8 @@
 from configs.config_reader import get_config
 from framework.chat_checker import ChatChecker
 from rm import Rm
+from datetime import datetime
+from datetime import date, timedelta
 
 config = get_config()
 
@@ -10,6 +12,23 @@ def get_task():
      #   return
     rm = Rm()
 
-    task = rm.redmine.issue.get(16)
+    task = rm.redmine.issue.get(4, include='journals')
+    
     for journal in task.journals.resources:
-        print(journal)
+       
+        for detail in journal['details']:
+            
+            if detail['name'] == 'status_id' and detail['new_value'] == '2':
+                time_work_1 = journal['created_on']
+                date_dt_1 = datetime.strptime(time_work_1, "%Y-%m-%dT%H:%M:%SZ")
+                print(date_dt_1)
+                print(type(date_dt_1))
+
+
+            if detail['name'] == 'status_id' and detail['old_value'] == '2' and  detail['new_value'] == '3':
+                time_work_2 =  journal['created_on']
+                date_dt_2 = datetime.strptime(time_work_2, "%Y-%m-%dT%H:%M:%SZ")
+
+
+    time_task = date_dt_2 - date_dt_1
+    print(time_task)
